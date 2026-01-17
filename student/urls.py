ff948e638e5c5ad69d15693ee49c439d04fc3348
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2026-01-17 08:55:32 +0545

    Feat: Add student list view

--- student/urls.py
+++ student/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.student_list, name='student-list'),
     path('add-student/', views.add_student, name='add-student'),
-    path('student-list/', views.student_list, name='student-list'),
 ]
