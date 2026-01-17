a98eea210161dedb4a2a81bd38985223da222ca3
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2026-01-17 22:09:42 +0545

    fix: prevent reverse relatio clash

--- home_auth/models.py
+++ home_auth/models.py
@@ -10,7 +10,24 @@ class CustomUser(AbstractUser):
     date_joined = models.DateTimeField(auto_now_add=True)
     is_authorized = models.BooleanField(default=False)
 
+    # user roles
     is_admin = models.BooleanField(default=False)
     is_student = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
 
+    # Set related_name to None to prevent reverse relationship creation
+    groups = models.ManyToManyField(
+        'auth.Group',               # Django's built-in Group model
+        related_name=None,          # Prevent reverse relationship
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',          # Django's built-in Permission model
+        related_name=None,          # Prevent reverse relationship
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
