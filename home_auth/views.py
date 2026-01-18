70512c13f49d21467d70443db22e6e5a991b703a
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2026-01-18 21:35:07 +0545

    feat: add authentication

--- home_auth/views.py
+++ home_auth/views.py
@@ -1,11 +1,48 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
 # Create your views here.
+from .models import CustomUser
 
 def signup_view(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        # handle existing user case
+        if CustomUser.objects.filter(email= email).exists():
+            return render(request, 'authentication/register.html', {'error': 'Email already in use. Please sign up with a different email or login.'})
+
+        # Handle password mismatch
+        if password != confirm_password:
+            return render(request, 'authentication/register.html', {'error': 'Passwords do not match.'})
+
+        # create user logic goes here
+        user = CustomUser.objects.create_user(  # create_user is a method provided by AbstractUser (ensure password is hashed & default fields are handled -> create won't work here)
+            username=email,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password
+        )
+        login(request, user)
+        return redirect('student_list')     # redirect to dashboard later based on user role
     return render(request, 'authentication/register.html')
 
+from django.contrib.auth import authenticate
 def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, username=email, password=password) # get the authenticated user object
+        if user is not None:
+            login(request, user)
+            return redirect('student_list')  # redirect to dashboard later based on user role
+        return render(request, 'authentication/login.html', {'error': 'Invalid email or password.'})
+
     return render(request, 'authentication/login.html')
 
 def forgot_password_view(request):
