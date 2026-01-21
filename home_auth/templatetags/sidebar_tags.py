ca4fbb7384b3cb69753627c78fa63a5eaab04dba
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2026-01-21 19:24:53 +0545

    feat: dynamic sidebar activation
    - django custom template tags
    - created templatetags directory (must) + added init.py to make it python package
    - wrote custom function for  template tag & registered as simple_tag using Django's template.Library()
    - loaded tag in template {% load sidebar_tags %}
    - use tag in sidebar {% is_active_menu 'url_name  %}

---
+++ home_auth/templatetags/sidebar_tags.py
@@ -0,0 +1,25 @@
+from django import template
+
+register = template.Library()   # for registering custom template tags and filters
+
+@register.simple_tag(takes_context=True)        # register a simple tag that takes the template context
+def is_active_menu(context, url_name):
+    """
+    Check if the given menu item is the active page.
+    Usage in template: {% is_active 'menu_item_name' %}
+    """
+
+    ''' 
+    # for static path matching
+    request = context['request']               # get the current request from the context
+    current_path = request.path                 # get the current path from the request
+    return 'active show' if current_path.startswith(f'/{menu_item}/') else ''
+    '''
+
+    # for dynamic path matching
+    current_url = context.request.resolver_match.url_name  # get the current URL name from the request context
+    print("Current URL Name:", current_url)
+    print("URL Name to Match:", url_name)
+
+    
+    return 'active' if current_url == url_name else ''
